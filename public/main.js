3a9db36b0f505b980ce341a3d05cacda39328fd1
Noto <notosgnikusnsi@gmail.com>
2024-05-01 00:50:39 +0900

    feat: pdfのテキストを抜き出す関数を作成する

--- public/main.js
+++ public/main.js
@@ -1,4 +1,26 @@
-window.onload = async () => {
-  const res = await fetch('/hello');
-  document.getElementById('message').innerText = await res.text();
-};
+import pdfjsDist from 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.2.67/+esm'
+
+const { pdfjsLib } = globalThis;
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.2.67/build/pdf.worker.mjs';
+
+async function getPdfText(pdfFilePath) {
+  const loadingTask = pdfjsLib.getDocument(pdfFilePath);
+
+  const pdf = await loadingTask.promise;
+  const maxPages = pdf.numPages;
+
+  let pdfText = "";
+
+  for (let i = 1; i <= maxPages; i++) {
+    const page = await pdf.getPage(i);
+    const content = await page.getTextContent({ disableCombineTextItems: false, includeMarkedContent: false });
+    const pageText = content.items.map((item) => ("str" in item ? item.str : "")).join("\n");
+    pdfText += pageText + "\n";
+  }
+
+  console.log(pdfText);
+
+  return pdfText;
+}
+
+
