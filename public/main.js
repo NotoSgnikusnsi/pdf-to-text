edcedcf17ba1fb22b22f3209d6c49af4124891b2
Noto <notosgnikusnsi@gmail.com>
2024-05-02 01:47:42 +0900

    feat: pdfをテキストに変換する関数にエラーバンドリングを追加し、htmlからpdfを指定できるようにする。

--- public/main.js
+++ public/main.js
@@ -1,26 +1,51 @@
 import pdfjsDist from 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.2.67/+esm'
 
 const { pdfjsLib } = globalThis;
+
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@4.2.67/build/pdf.worker.mjs';
 
 async function getPdfText(pdfFilePath) {
-  const loadingTask = pdfjsLib.getDocument(pdfFilePath);
+  try {
+    const loadingTask = pdfjsLib.getDocument(pdfFilePath);
 
-  const pdf = await loadingTask.promise;
-  const maxPages = pdf.numPages;
+    const pdf = await loadingTask.promise;
+    const maxPages = pdf.numPages;
 
-  let pdfText = "";
+    let pdfText = "";
 
-  for (let i = 1; i <= maxPages; i++) {
-    const page = await pdf.getPage(i);
-    const content = await page.getTextContent({ disableCombineTextItems: false, includeMarkedContent: false });
-    const pageText = content.items.map((item) => ("str" in item ? item.str : "")).join("\n");
-    pdfText += pageText + "\n";
+    for (let i = 1; i <= maxPages; i++) {
+      const page = await pdf.getPage(i);
+      const content = await page.getTextContent({ disableCombineTextItems: false, includeMarkedContent: false });
+      const pageText = content.items.map((item) => ("str" in item ? item.str : "")).join("\n");
+      pdfText += pageText + "\n";
+    }
+
+    return pdfText;
+
+  } catch (error) {
+    console.error("PDFの読み込みに失敗しました: ", error);
+    throw error;
   }
-
-  console.log(pdfText);
-
-  return pdfText;
 }
 
+document.getElementById("button").addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const textarea = document.getElementById("textarea");
+  const file = document.getElementById("inputFile").files[0];
+
+  if (file) {
+    if (file.type !== "application/pdf") {
+      alert("PDFファイルを指定してください");
+      return;
+    }
+    const fileUrl = window.URL.createObjectURL(file);
+    const text = await getPdfText(fileUrl);
+    textarea.value = text;
+  } else {
+    alert("ファイルを指定してください");
+    return;
+  }
+
+})
 
